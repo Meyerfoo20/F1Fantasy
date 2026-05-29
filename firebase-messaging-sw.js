4b15bb53d579764ced16dd9127a31eda286173f4
Meyerfoo20 <158509755+Meyerfoo20@users.noreply.github.com>
2026-05-29 21:13:48 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAskluSUlzitU5eW6iQ6Jrw6GTXMtlu0-c",
+    authDomain: "f1-fantasy-e8204.firebaseapp.com",
+    projectId: "f1-fantasy-e8204",
+    storageBucket: "f1-fantasy-e8204.firebasestorage.app",
+    messagingSenderId: "952247160192",
+    appId: "1:952247160192:web:57bd2653326d5c7c7ec565"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('Background message received:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'logo.jpg',
+        badge: 'logo.jpg'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
